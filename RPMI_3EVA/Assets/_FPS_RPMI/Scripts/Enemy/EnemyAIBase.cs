7a247849b8b072200a1796584e5bdc35b9f331c4
Claramh-13 <claramonasterioh@gmail.com>
2026-03-12 12:38:30 +0100

    seguimiento enemigo

---
+++ RPMI_3EVA/Assets/_FPS_RPMI/Scripts/Enemy/EnemyAIBase.cs
@@ -0,0 +1,144 @@
+using UnityEngine;
+using UnityEngine.AI; //Librería de componentes NavMesh
+
+public class EnemyAIBase : MonoBehaviour
+{
+    #region General Variables
+    [Header("AI Configuration")]
+    [SerializeField] NavMeshAgent agent; //Ref al cerebro del agente
+    [SerializeField] Transform target; //Ref al target a perseguir (variable)
+    [SerializeField] LayerMask targetLayer; //Define layer del target (Detecciones)
+    [SerializeField] LayerMask groundLayer; //Define layer del suelo (Evita ir a zonas sin suelo)
+
+    [Header("Patroling Stats")]
+    [SerializeField] float walkPointRange = 10f; //Radio máximo para determinar puntos a perseguir
+    Vector3 walkPoint; //posición del punto random a perseguir
+    bool walkPointSet; //¿Hay punto a perseguir generado? Si es false, genera uno
+
+    [Header("Attacking Stats")]
+    [SerializeField] float timeBetweenAttacks = 1f; //Cooldown entre ataques
+    [SerializeField] GameObject projectile; //Ref a la bala física que dispara el enemigo
+    [SerializeField] Transform shootPoint; //Posición desde la que se dispara la bala
+    [SerializeField] float shootSpeedY; //Fuerza de disparo hacia arriba (Catapulta)
+    [SerializeField] float shootSpeedZ = 10f; //Fuerza de disparo hacia delante (siempre está)
+    bool alreadyAttacked; //Si es verdadero no stackea ataques y entra en espera entre ataques
+
+    [Header("States & Detection")]
+    [SerializeField] float sightRange = 8f; //Radio del detector de persecución
+    [SerializeField] float attackRange = 2f; //Radio del detector de ataque
+    [SerializeField] bool targetInSightRange; //Determina si es verdadero que podemos perseguir al target
+    [SerializeField] bool targetInAttackRange; //Determina si es verdadero que podemos atacar al target
+
+    [Header("Stuck Detection")]
+    [SerializeField] float stuckCheckTime = 2f; //Tiempo que el agente espera estando quieto antes de darse cuenta de que está stuck
+    [SerializeField] float stuckThreshold = 0.1f; //Margen de detección de stuck
+    [SerializeField] float maxStuckDuration = 3f; //Tiempo máximo de estar stuck
+
+    float stuckTimer; //Reloj que cuenta el tiempo de estar stuck
+    float lastCheckTime; //Tiempo de chequeo previo de stuck
+    Vector3 lastPosition; //Posición del último walkpoint perseguido
+    #endregion
+
+    private void Awake()
+    {
+        target = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();   
+        lastPosition = transform.position;
+        lastCheckTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyStateUpdater();
+    }
+
+    void EnemyStateUpdater()
+    {
+        //Metodo que se encarga de gestioanr el cambio de estados del enemigo
+
+        //1 - El cambio de estado de los bools
+        //Primero detetamos si los targets estan en vision
+        Collider[] hits = Physics.OverlapSphere(transform.position, sightRange, targetLayer);
+        targetInSightRange = hits.Length > 0;  
+        //Segundo, si estan en visión detetctamos si ademas estan en ataque
+        if (targetInSightRange)
+        {
+            float distance = Vector3.Distance(transform.position, target.position);
+            targetInAttackRange = distance <= attackRange;
+        }
+        else
+        {
+            targetInAttackRange = false;    
+        }
+
+
+        //2 - Cambio de estados segun booleanos
+        if (!targetInSightRange && !targetInAttackRange)
+        {
+            Patroling();
+        }
+        else if (targetInSightRange && !targetInAttackRange)
+        {
+            ChaseTarget();
+        }
+        else if (targetInSightRange && targetInAttackRange)
+        {
+            AttackTarget();
+        }
+    }
+
+
+    void Patroling()
+    {
+        Debug.Log("Enemigo en estado de patrulla");
+    }
+
+    void ChaseTarget()
+    {
+        //Accion que le dije al agente que persiga al target
+        agent.SetDestination(target.position);
+    }
+
+    void AttackTarget()
+    {
+        //Accion que contiene la lógica de ataque
+        //1- Hacer que el agente se quede quieto, es decir, que se perdiga a si mismo
+        agent.SetDestination(transform.position);
+        //2- Aplicar una rotación suavizzada para que el agente mire al target antes de atacar
+        Vector3 direction = (target.position - transform.position).normalized;
+        if (direction != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, agent.angularSpeed * Time.deltaTime);
+        }
+
+        //3- Se ataca(solo si no se esta atacando)
+        if (!alreadyAttacked)
+        {
+            Rigidbody rb = Instantiate(projectile, shootPoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * shootSpeedZ, ForceMode.Impulse);
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+
+
+    }
+
+    void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (Application.isPlaying) return; //si esamos jugando en build no se ejecuta el resto del código
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+
+}
--- RPMI_3EVA/Assets/_FPS_RPMI/Scripts/Player/GunSystem.cs
+++ RPMI_3EVA/Assets/_FPS_RPMI/Scripts/Player/GunSystem.cs
@@ -1,71 +1,66 @@
-using UnityEngine; 
-using UnityEngine.InputSystem;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GunSystem : MonoBehaviour
 {
-
     #region General Variables
     [Header("General References")]
-    [SerializeField] Camera fpsCam; //Referencia si disparamos desde el centro de la cam
-    [SerializeField] Transform shootPoints; //Ref si queremos disparar desde la punta del cañon
-    [SerializeField] LayerMask impactLayer; //Layer con la que el RayCast interactua
-    RaycastHit hit; //Almacen de la información de los objetos a los que el raycast puede impactar
+    [SerializeField] Camera fpsCam; //Ref si disparamos desde el centro de la cam
+    [SerializeField] Transform shootPoint; //Ref si queremos disparar desde la punta del cañón
+    [SerializeField] LayerMask impactLayer; //Layer con la que el Raycast interactúa
+    RaycastHit hit; //Almacén de la información de los objetos a los que el Raycast puede impactar
 
-    [Header("Weapon parameters")]
-    [SerializeField] int damage = 10; //daño del arma por bala
-    [SerializeField] float range = 100f; //Distancia del disparo
-    [SerializeField] float spread = 0; //Radio de dispersion del arma
+    [Header("Weapon Parameters")]
+    [SerializeField] int damage = 10; //Daño del arma por bala
+    [SerializeField] float range = 100f; //Distancia de disparo
+    [SerializeField] float spread = 0; //Radio de dispersión del arma
     [SerializeField] float shootingCooldown = 0.2f; //Tiempo entre disparos
-    [SerializeField] float reloadTime = 1.5f; //Tiempo de recarga
-    [SerializeField] bool allowButtonHold = false; //Si el disparo se ejecuta por click(falso) o mantener(true)
+    [SerializeField] float reloadTime = 1.5f; //Tiempo de recarga en segundos
+    [SerializeField] bool allowButtonHold = false; //Si el disparo se ejecuta por click (false) o por mantener (true)
 
     [Header("Bullet Management")]
-    [SerializeField] int ammoSize = 30; //Cantidad max de balas por cargador
-    [SerializeField] int bulletsPerTap = 1; //Cantidad de balas disparadas por cada ejecucion de disparo
+    [SerializeField] int ammoSize = 30; //Cantidad max de balas/cargador
+    [SerializeField] int bulletsPerTap = 1; //Cantidad de balas disparadas por cada ejecución de disparo
     int bulletsLeft; //Cantidad de balas dentro del cargador actual
 
     [Header("Feedback References")]
-    [SerializeField] GameObject impactEffect; //Referencia al VFX de impacto de la bala
+    [SerializeField] GameObject impactEffect; //Ref al VFX de impacto de bala
 
     [Header("Dev - Gun State Bools")]
-    [SerializeField] bool shooting; //Indica si estamos disparado
+    [SerializeField] bool shooting; //Indica si estamos disparando
     [SerializeField] bool canShoot; //Indica si podemos disparar en X momento del juego
     [SerializeField] bool reloading; //Indica si estamos en proceso de recarga
-
     #endregion
 
     private void Awake()
     {
-        bulletsLeft = ammoSize; // Al iniciar la partida tenemos el cargador lleno
-        canShoot = true; //Al inciar la partida, tenemos la poisibilidad de disparar
+        bulletsLeft = ammoSize; //Al iniciar la partida, tenemos el cargador lleno
+        canShoot = true; //Al iniciar la partida, tenemos la posibilidad de disparar
     }
-
-  
 
     // Update is called once per frame
     void Update()
     {
-        //Condición estricta de llamar a la corrutina de diaparo
+        //Condición estricta de llamar a la rutina de disparo
         if (canShoot && shooting && !reloading && bulletsLeft > 0)
         {
             StartCoroutine(ShootRoutine());
         }
-
     }
 
     IEnumerator ShootRoutine()
     {
-        //La corrutina se va a encargar de medir el tiempo entre diaparos y la gestión del gasto de balas
-        //Ademas llamará al raycast de disparo que está definido en Shoot()ç
+        //La corrutina se va a encarga de medir el tiempo entre disparos y la gestión del gasto de balas
+        //Además llamará al raycast de disparo que está definido en Shoot()
 
-        canShoot = false; //LLave de seguridadque hace que si estamos disparando no podamos disparar
+        canShoot = false; //Llave de seguridad que hace que si estamos disparando no podamos disparar
         if (!allowButtonHold) shooting = false; //Cerrar el bucle de disparo por pulsación
-        for(int i = 0;i < bulletsLeft; i++)
+        for (int i = 0; i < bulletsPerTap; i++)
         {
-            if (bulletsLeft <= 0) break; //Segunda prevención de errores:si no me quedan balas no hago daño
-            Shoot(); //Llamada al raycast que define el disparo
-            bulletsLeft--; //Resta a la cantidad de balas del cargador actual
+            if (bulletsLeft <= 0) break; //Segunda prevención de errores: si no me quedan balas no hago daño
+            Shoot(); //Llamada al Raycast que define el disparo
+            bulletsLeft--; //Resta 1 a la cantidad de balas del cargador actual
         }
 
         //Espera entre disparos
@@ -75,21 +70,27 @@ public class GunSystem : MonoBehaviour
 
     void Shoot()
     {
-        //ESTE ES EL ÉTODO MAS IMPORTANTE
+        //ESTE ES EL MÉTODO MÁS IMPORTANTE
         //AQUÍ SE DEFINE EL DISPARO POR RAYCAST = UTILIZABLE CON CUALQUIER MECÁNICA
 
-        //Almacebar la dirección de diapro y modificarla en caso de haber dispersion(spread)
+        //Almacenar la dirección de disparo y modificarla en caso de haber spread
         Vector3 direction = fpsCam.transform.forward; //Se lanza rayo hacia delante de la cámara
-        //Añadir dispersión aleatoria segun el valor de dispersion(spread)
+        //Añadir dispersión aleatoria según el valor de spread
         direction.x += Random.Range(-spread, spread);
         direction.y += Random.Range(-spread, spread);
 
         //DECLARACIÓN DEL RAYCAST
-        //Physics.Raycast(Origen del rayo, dirección, almacen de la info del impacto, longitud del rayo, layer con la que impacta el rayo))
+        //Physics.Raycast(Origen del rayo, dirección, almacén de la info del impacto, longitud del rayo, layer con la que impacta el rayo)
         if (Physics.Raycast(fpsCam.transform.position, direction, out hit, range, impactLayer))
         {
-            //AQUÍ PUEDO CODEAR TODOS LOS EFECTOS DEL RAYO QUE QUIERO PARA MI INTERACCIÓN
+            //AQUÍ PUEDO CODEAR TODOS LOS EFECTOS QUE QUIERO PARA MI INTERACCIÓN
             Debug.Log(hit.collider.name);
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+                enemyHealth.TakeDamage(damage);
+            }
+
         }
     }
 
@@ -100,33 +101,30 @@ public class GunSystem : MonoBehaviour
 
     IEnumerator ReloadRoutine()
     {
-       reloading = true; //Estamos recargando, por lo tanto no podemos recargar
-       //AQUÍ LLAMARIAMOS A LA ANIMACIÓN DE RECARGA
-       yield return new WaitForSeconds(reloadTime); //Esperar x tiempo que es lo que dura la animación de recarga
+        reloading = true; //Estamos recargando, por lo tanto no podemos recargar
+        //AQUÍ LLAMARIAMOS A LA ANIMACIÓN DE RECARGA
+        yield return new WaitForSeconds(reloadTime); //Esperar tanto tiempo como dura la animación de recarga
         bulletsLeft = ammoSize; //La cantidad de balas actuales se iguala a la máxima
-        reloading = false; //termina la recarga, podemos volver a recargar
+        reloading = false; //Termina la recarga, podemos volver a recargar
     }
-
 
     #region Input Methods
     public void OnShoot(InputAction.CallbackContext context)
     {
         if (allowButtonHold)
         {
-            shooting = context.ReadValueAsButton(); //Detecta constantemente si el boton de disparo está apretado
+            shooting = context.ReadValueAsButton(); //Detecta constantemente si el botón de disparo está apretado
         }
         else
         {
             if (context.performed) shooting = true; //Shooting solo es true por pulsación
         }
     }
+
     public void OnReload(InputAction.CallbackContext context)
     {
-        if (context.performed) Reload();   
+        if (context.performed) Reload();
     }
-
     #endregion
-
-
 
 }
